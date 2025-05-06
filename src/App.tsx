169bd855bae9a7d557678223f0ab847fac398307
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2025-05-05 21:51:21 -0300

    Feat: Adiciona Shadcn/ui e Lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./global.css";
 
 export const App = () => {
-  return <div className="bg-teal-500 pb-4">App</div>;
+  return (
+    <div className="bg-teal-500 pb-4">
+      <span>App</span>
+    </div>
+  );
 };
